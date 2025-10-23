eaf34e9aadb4bacbeb6dbdf7b08a8c41c9cdd04e
arzharch <arzharch@gmail.com>
2025-10-23 13:17:34 +0530

    create simple post request

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 
 app=FastAPI()
@@ -6,3 +8,9 @@ app=FastAPI()
 @app.get("/")
 async def root():
     return "Hello, World!"
+
+
+@app.post("/create_posts")
+async def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return { "new_post":f"title {payLoad['title']} and the content is {payLoad['content']}"}

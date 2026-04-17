469863c8366aa05d94e7443082c92c403cdc4e27
arzharch <arzharch@gmail.com>
2026-04-18 01:39:03 +0530

    add tags to routers

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,9 +5,9 @@ from ..database import SessionLocal, get_db
 from sqlalchemy.orm import Session
  
 
-router=APIRouter()
+router=APIRouter(prefix="/users", tags=['Users'])
 
-@router.post("/users",status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+@router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db:Session=Depends(get_db)):
 
     user.password=utils.password_hash.hash(user.password)
@@ -19,7 +19,7 @@ def create_user(user: schemas.UserCreate, db:Session=Depends(get_db)):
 
     return new_user
 
-@router.get("/users/{id}", response_model=schemas.User, status_code=status.HTTP_200_OK)
+@router.get("/{id}", response_model=schemas.User, status_code=status.HTTP_200_OK)
 async def get_user(id : int, db:Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id==id).first()
     if not user:
